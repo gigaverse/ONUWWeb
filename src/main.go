c289567927cff3c9da4696c4ccbbb4c13621b47d
gigaverse <youssefpower1997@gmail.com>
2016-10-02 00:27:07 -0500

    Actually functioning base game

--- src/main.go
+++ src/main.go
@@ -2,34 +2,53 @@ package main
 
 import (
 	"fmt"
-	"html/template"
-	"log"
-	"net/http"
-	"strings"
+	//	"html/template"
+	//	"log"
+	"math/rand"
+	//	"net/http"
+	//	"strings"
 )
 
 var Decks map[int][]card
 
-const (
-	Werewolf     card = card{"Werewolf", "ww"}
-	Mystic       card = card{"Mystic Wolf", "ww"}
-	Robber       card = card{"Robber", "v"}
-	Tanner       card = card{"Tanner", "t"}
-	Drunk        card = card{"Drunk", "v"}
-	DG           card = card{"Doppleganger", "v"}
-	Insomniac    card = card{"Insomniac", "v"}
-	Seer         card = card{"Seer", "v"}
-	APSeer       card = card{"Apprentice Seer", "v"}
-	ParaInv      card = card{"Paranormal Investigator", "v"}
-	Witch        card = card{"Witch", "v"}
-	Troublemaker card = card{"Troublemaker", "v"}
-	Minion       card = card{"Minion", "v"}
-	Alpha        card = card{"Alpha Wolf", "ww"}
-	Mason        card = card{"Mason", "v"}
-)
+var NoRole card
+var Werewolf card
+var Mystic card
+var Robber card
+var Tanner card
+var Drunk card
+var DG card
+var Insomniac card
+var Seer card
+var APSeer card
+var ParaInv card
+var Witch card
+var Troublemaker card
+var Minion card
+var Alpha card
+var Mason card
+var Villager card
 
 // Different Decks depending on player count
-func init() {
+func start() {
+	NoRole = card{"NOROLE", "xxx"}
+	Werewolf = card{"Werewolf", "ww"}
+	Mystic = card{"Mystic Wolf", "ww"}
+	Robber = card{"Robber", "v"}
+	Tanner = card{"Tanner", "t"}
+	Drunk = card{"Drunk", "v"}
+	DG = card{"Doppleganger", "v"}
+	Insomniac = card{"Insomniac", "v"}
+	Seer = card{"Seer", "v"}
+	APSeer = card{"Apprentice Seer", "v"}
+	ParaInv = card{"Paranormal Investigator", "v"}
+	Witch = card{"Witch", "v"}
+	Troublemaker = card{"Troublemaker", "v"}
+	Minion = card{"Minion", "v"}
+	Alpha = card{"Alpha Wolf", "ww"}
+	Mason = card{"Mason", "v"}
+	Villager = card{"Villager", "v"}
+	Decks = make(map[int][]card)
 	Decks[3] = []card{Werewolf, Werewolf, Robber, Tanner, Drunk, DG}
 	Decks[4] = []card{Werewolf, Werewolf, Robber, Tanner, Drunk, DG, Troublemaker}
 	Decks[5] = []card{Werewolf, Seer, Insomniac, Mystic, APSeer, ParaInv, Witch, Troublemaker}
@@ -38,9 +57,10 @@ func init() {
 	Decks[8] = []card{Werewolf, Werewolf, Villager, Villager, Seer, Robber, Troublemaker, Tanner, Minion, Mason, Mason}
 	Decks[9] = []card{Mystic, Werewolf, Robber, Troublemaker, Tanner, Minion, DG, Mason, Mason, ParaInv, Seer, Drunk}
 	Decks[10] = []card{Robber, Troublemaker, Tanner, Minion, DG, Alpha, Mystic, APSeer, ParaInv, Mason, Mason, Seer, Drunk}
+
 }
 
-func login(w http.ResponseWriter, r *http.Request) {
+/*func login(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("method:", r.Method) //get request method
 	if r.Method == "GET" {
 		t, _ := template.ParseFiles("login.gtpl")
@@ -51,12 +71,21 @@ func login(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("username:", r.Form["username"])
 		fmt.Println("password:", r.Form["password"])
 	}
-}
+}*/
 func main() {
-	http.HandleFunc("/login", login)
+	/*http.HandleFunc("/login", login)
 	err := http.ListenAndServe(":9090", nil) // setting listening port
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
+	}*/
+	start()
+	players := []player{player{NoRole, "John"}, player{NoRole, "James"}, player{NoRole, "Jimmy"}}
+	for j := 0; j < 10; j++ {
+		middle := AssignRoles(players)
+		for i := 0; i < len(players); i++ {
+			fmt.Printf("%s has %s card\n", players[i].name, players[i].role)
+		}
+		fmt.Printf("%v is the deck\n", middle)
 	}
 }
 
@@ -72,16 +101,16 @@ type player struct {
 	name string
 }
 
-func (p *player) CheckRole() string {
+func (p *player) CheckResults() string {
 	//killed player was a werewolf, village victory
-	if p.card.team == "ww" {
-		return "w"
+	if p.role.team == "ww" {
+		return "v"
 		//killed player was a tanner, tanner victory
-	} else if p.card.team == "t" {
+	} else if p.role.team == "t" {
 		return "t"
 		//killed player was something else, werewolf victory
 	} else {
-		return "v"
+		return "w"
 	}
 
 }
@@ -91,7 +120,22 @@ func SwapCards(x, y card) (card, card) {
 }
 
 //Called at the start of the round to give players their cards TODO
-func AssignRoles(players []player) {
+func AssignRoles(players []player) []card {
 	var deck []card
+	var middle []card
+	if len(players) < 3 {
+		fmt.Printf("OI you  don't have enough players! %d", len(players))
+		return middle
+	}
 	deck = Decks[len(players)]
+	CardOrder := rand.Perm(len(deck))
+	//Assigns each player a random card #THIS SHOULD MAKE NEW CARDS INSTEAD
+	//OF SCRAMBLING THE PRESETS
+	for i := 0; i < len(players); i++ {
+		players[i].role = card{deck[CardOrder[i]].name, deck[CardOrder[i]].team}
+	}
+	//Puts the rest of the cards in the middle
+	//TODO -- Fix this shit
+	middle = deck[len(players):]
+	return middle
 }
